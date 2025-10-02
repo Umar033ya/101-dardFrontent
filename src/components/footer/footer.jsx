b216c09cdc395f5945aaf7253b31479a450d126d
Umar033ya <umaryakubov033ppp@gmail.com>
2025-10-02 17:52:49 +0500

    header footer and stn1 add

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './footer.css'
-
+import footerLogo from '../../../public/footerLogo.png'
 function Footer() {
 
   return (
     <div className='body'>
-<h1>Salom</h1>
+     <footer>
+    <div className="upper">
+      <img src={footerLogo} alt="" />
+      <p>Virtual Class for Zoom</p>
+    </div>
+    <h2>Subscribe to get our Newsletter</h2>
+    <form className='form'>
+      <input placeholder='Your Email' type="email" />
+      <button>Subscribe</button>
+    </form>
+    <ul>
+      <li><a href="#">Careers</a></li>
+      <li><a href="#">Privacy Policy</a></li>
+      <li><a href="#">Terms & Conditions</a></li>
+    </ul>
+    <h3>© 2021 Class Technologies Inc. </h3>
+</footer>
     </div>
   )
 }
