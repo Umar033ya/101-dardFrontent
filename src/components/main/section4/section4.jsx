f044b1e271cde6ee9e1fd79ee27506402752ad3b
abduboriyfr <abduboriyking10@gmail.com>
2025-10-04 17:37:39 +0500

    stn4 and stn 5 add

--- src/components/main/section4/section4.jsx
+++ src/components/main/section4/section4.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './section4.css'
+import sect4Img from"./Group 122.png"
 
 function Section4() {
 
   return (
     <div className='body'>
-<h1>Salom</h1>
-
+<div className="section4">
+  <div className="sect4-text">
+  <h3>Our <span>Features</span></h3>
+  <p>This very extraordinary feature, can make learning activities more efficient</p>
+  </div>
+  <div className="sect4-next">
+<div className="sect4-next-t">
+<h3><span>Tools</span> For Teachers And Learners</h3>
+    <p>Class has a dynamic set of teaching tools built to be deployed and used during class.
+    Teachers can handout assignments in real-time for students to complete and submit.</p>
+</div>
+<div className="sect4-next-img">
+<img src={sect4Img} alt="rasm" />
+</div>
+  </div>
+</div>
     </div>
   )
 }
--- src/components/main/section5/section5.jsx
+++ src/components/main/section5/section5.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './section5.css'
+import sect5 from"./Group 92.png"
 
 function Section5() {
 
   return (
     <div className='body'>
-<h1>Salom</h1>
-
+<div className="section5">
+  <div className="sect5-left">
+    <img src={sect5} alt="rasm" />
+  </div>
+  <div className="sect5-right">
+    <h3>Assessments, <span>Quizzes</span>, Tests</h3>
+    <p>Easily launch live assignments, quizzes, and tests.
+    Student results are automatically entered in the online gradebook.</p>
+  </div>
+</div>
     </div>
   )
 }
